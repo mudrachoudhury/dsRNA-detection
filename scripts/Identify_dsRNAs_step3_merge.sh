4a887729ac557a8775beaaba7534e093881d037c
Mudra Choudhury <mudrachoudhury@users.noreply.github.com>
2023-11-29 15:31:12 -0800

    Update Identify_dsRNAs_step3_merge.sh

--- scripts/Identify_dsRNAs_step3_merge.sh
+++ scripts/Identify_dsRNAs_step3_merge.sh
@@ -1,39 +1,55 @@
 #!/bin/bash
-#SBATCH -o Identify_dsRNAs_step3.out
-#SBATCH -e Identify_dsRNAs_step3.err
-#SBATCH -p medium
-#SBATCH --mem 12G
-#SBATCH -V
+#SBATCH -o Identify_dsRNAs_step3.out # Standard output will be written to Identify_dsRNAs_step3.out
+#SBATCH -e Identify_dsRNAs_step3.err # Standard error will be written to Identify_dsRNAs_step3.err
+#SBATCH -p medium                # Specifies the partition (or queue) to submit the job
+#SBATCH --mem 12G                # Memory requirement: allocate 12G of RAM for this job
+#SBATCH -V                       # Exports the current environment to the job environment
 
-ja=Identify_dsRNAs_step3.job
-job_name=Identify_dsRNA_step3
+# Variable definitions
+ja=Identify_dsRNAs_step3.job     # Job array file name
+job_name=Identify_dsRNA_step3    # Name of the job
 
-#This script merges windows of regions that are within certain distances of each other. Then the script increases the EES regions by 50 bp on each side and merges overlapping regions.
+# Script description:
+# This script merges windows of regions that are within certain distances of each other. 
+# Then the script increases the EES regions by 50 bp on each side and merges overlapping regions.
+
+# Extracting the task ID from the SLURM array job
 idx=${SLURM_ARRAY_TASK_ID}
 
-PARMS=($(awk "NR==$idx" $ja)) #task ID is the line of the job file (aka job number)
-input_fns=${PARMS[0]}
+# Reading parameters from the job array file
+PARMS=($(awk "NR==$idx" $ja))    # Extract the line corresponding to the task ID from the job file
+input_fns=${PARMS[0]}            # Assign the first parameter from the line as input filenames
 
+# Logging start time and job information
 echo -e "Job begin: $(date)\n" 1>>log/$job_name.$idx.out 2>>log/$job_name.$idx.err
 
-#input_fns=$(ls ../data/All_EES_*sorted*.bed)
+# Declare an array of distances for merging windows
 declare -a distances=("50" "100" "150" "200" "400" "600" "800" "1000" "1500" "2000")
 
+# Loop over each input file and perform operations
 for i in $input_fns; do
     for n in "${distances[@]}"; do
+        # Preparing filenames for output and temporary files
         new_file=$(echo $i | sed -e 's@.*/@@' )
         output_fn="../data/Merged_"$n"_dist_"$new_file
         temp_fn1=$output_fn'_temp1.bed'
-        temp_fn2=$output_fn'_temp2.bed'
+        
+        # Log the output file name
         echo output file: $output_fn 1>>log/$job_name.$idx.out
-        ~/software/bedtools2/bin/mergeBed -d $n -s -i $i > $temp_fn1 2>>log/$job_name.$idx.err #We first merge all regions within a given distance together and overlapping regions
-        awk -v OFS='\t' '{print $1, $2-50, $3+50, $4}' $temp_fn1 > $output_fn 2>>log/$job_name.$idx.err #We increase the length of each region by 50bp before and 50bp after the start and end
-        #~/software/bedtools2/bin/mergeBed -s -i $temp_fn2 > $output_fn 2>>log/$job_name.$idx.err #We again merge the any regions that are now overlapping
+
+        # Merge regions within a given distance using bedtools
+        ~/software/bedtools2/bin/mergeBed -d $n -s -i $i > $temp_fn1 2>>log/$job_name.$idx.err
+
+        # Increase the length of each region by 50bp at the start and end
+        awk -v OFS='\t' '{print $1, $2-50, $3+50, $4}' $temp_fn1 > $output_fn 2>>log/$job_name.$idx.err
+
+        # Clean up temporary files
         rm $temp_fn1
-        #rm $temp_fn2
+
+        # Log completion of processing for the current distance
         echo Done 1>>log/$job_name.$idx.out
     done
 done
 
+# Log job completion time
 echo -e "Job completed: $(date)\n" 1>>log/$job_name.$idx.out 2>>log/$job_name.$idx.err
-
